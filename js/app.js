1cd8c5dd3f6b664aed379f80830a656aa6bc849d
Manoj Talagadadeevi <manojt.854@gmail.com>
2016-12-02 22:15:39 -0500

    Passing one cat as a json object

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-var Cat = function() {
+var Cat = function(data) {
     "use strict";
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable("Tabby");
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/');
-    this.nickNames = ko.observableArray(['Tab-Tab', 'T-Bone', 'Mr. T']);
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nickNames = ko.observableArray(data.nickNames);
     this.catLevel = ko.computed( function() {
         let title, numClicks;
         numClicks = this.clickCount();
@@ -19,7 +19,13 @@ var Cat = function() {
 var ViewModel = function () {
     "use strict";
     const self = this;
-    this.currentCat = ko.observable(new Cat());
+    this.currentCat = ko.observable( new Cat({
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+        nickNames: ['Tab-Tab', 'T-Bone', 'Mr. T']
+    }) );
     this.incrementCounter = function () {
         // this.clickCount( this.clickCount() + 1 );
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
