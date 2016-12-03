8572957ed2987c43ea10448042671062caf0c312
Manoj Talagadadeevi <manojt.854@gmail.com>
2016-12-02 21:25:59 -0500

    Add Cat Levels to Cat Clicker

--- js/app.js
+++ js/app.js
@@ -8,6 +8,17 @@ var ViewModel = function () {
     this.incrementCounter = function () {
         this.clickCount( this.clickCount() + 1 );
     };
+
+    this.catLevel = ko.computed( function() {
+        let title, numClicks;
+        numClicks = this.clickCount();
+        if (numClicks < 10) { title = 'NewBorn'; }
+        else if (numClicks < 15) { title = 'Infant'; }
+        else if (numClicks < 20) { title = 'Child'; }
+        else if (numClicks < 25) { title = 'Adult'; }
+        else { title = 'Ninja'; }
+        return title;
+    }, this);
 };
 
 ko.applyBindings(new ViewModel());
